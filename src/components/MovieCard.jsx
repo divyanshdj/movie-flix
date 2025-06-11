ee940f51cafbf930d2ba60570e6cf45b1d947f87
Divyansh Jain <djboss8834@gmail.com>
2025-06-11 09:09:36 +0530

    Created : Movie Card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 
-const MovieCard = ({movie:{title, release_date}}) => {
+const MovieCard = ({movie:{title, release_date, original_language, poster_path, vote_average}}) => {
   return (
-    <div>
-        <p className="text-white">{title}</p>
-        <p className="text-yellow-100">{release_date}</p>
+    <div className="movie-card text-white">
+      <img
+        src={poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : '../assets/no-poster.png'}
+        alt={title}
+      />
+      <div className="mt-4 movie-details">
+        <h3 className="movie-title">{title}</h3>
+        <div className='content'>
+          <div className="rating">
+            <img src="star.svg" alt="Star Icon" />
+            <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
+          </div>
+          <span>•</span>
+          <p className='lang'>{original_language ? original_language : 'N/A'}</p>
+          <span>•</span>
+          <p className='year'>{release_date ? release_date.split('-')[0] : 'N/A'}</p>
+        </div>
+      </div>
     </div>
   )
 }
